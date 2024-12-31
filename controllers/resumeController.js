f58230c3f6c789ef62c646c5d8b2abe0cb7f54ae
Ankit Niranjan <ankitniranjan331@gmail.com>
2024-12-31 21:45:52 +0530

    ResumeDetailsAdded

--- controllers/resumeController.js
+++ controllers/resumeController.js
@@ -70,3 +70,189 @@ exports.deleteJobs = catchAsyncErrors(async (req, res, next) => {
   await student.save();
   res.json({ message: "Job Deleted" });
 });
+
+exports.addInternship = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  student.resume.internships.push({ ...req.body, id: uuidv4() });
+  await student.save();
+  res.json({ message: "Internship" });
+});
+
+exports.updateInternship = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  const internshipIndex = student.resume.internships.findIndex(
+    (i) => i.id === req.params.internshipid
+  );
+  student.resume.internships[internshipIndex] = {
+    ...student.resume.internships[internshipIndex],
+    ...req.body,
+  };
+  console.log(internshipIndex);
+  await student.save();
+  res.json({ message: "Internship Updated" });
+});
+
+exports.deleteInternship = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  const filteredInternships = student.resume.internships.filter(
+    (i) => i.id !== req.params.internshipid
+  );
+  student.resume.internships = filteredInternships;
+  await student.save();
+  res.json({ message: "Internship Deleted" });
+});
+
+exports.addResponsibilities = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  student.resume.responsibilities.push({ ...req.body, id: uuidv4() });
+  await student.save();
+  res.json({ message: "Responsibilities" });
+});
+
+exports.updateResponsibilities = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  const responsibilitiesIndex = student.resume.responsibilities.findIndex(
+    (i) => i.id === req.params.responsibilitiesid
+  );
+  student.resume.responsibilities[responsibilitiesIndex] = {
+    ...student.resume.responsibilities[responsibilitiesIndex],
+    ...req.body,
+  };
+  console.log(responsibilitiesIndex);
+  await student.save();
+  res.json({ message: "Responsibilities Updated" });
+});
+
+
+exports.deleteResponsibilities = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  const filteredResponsibilities = student.resume.responsibilities.filter(
+    (i) => i.id !== req.params.responsibilitiesid
+  );
+  student.resume.responsibilities=filteredResponsibilities
+  await student.save();
+  res.json({ message: "Responsibilities Deleted" });
+});
+
+exports.addCourses = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  student.resume.courses.push({ ...req.body, id: uuidv4() });
+  await student.save();
+  res.json({ message: "Courses" });
+});
+
+exports.updateCourses = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  const coursesIndex = student.resume.courses.findIndex(
+    (i) => i.id === req.params.coursesid
+  );
+  student.resume.courses[coursesIndex] = {
+    ...student.resume.courses[coursesIndex],
+    ...req.body,
+  };
+  console.log(coursesIndex);
+  await student.save();
+  res.json({ message: "Courses Updated" });
+});
+
+
+exports.deleteCourses = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  const filteredCourses = student.resume.courses.filter(
+    (i) => i.id !== req.params.coursesid
+  );
+  student.resume.courses=filteredCourses
+  await student.save();
+  res.json({ message: "Courses Deleted" });
+});
+
+exports.addProjects = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  student.resume.projects.push({ ...req.body, id: uuidv4() });
+  await student.save();
+  res.json({ message: "Projects" });
+});
+
+exports.updateProjects = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  const projectsIndex = student.resume.projects.findIndex(
+    (i) => i.id === req.params.projectsid
+  );
+  student.resume.projects[projectsIndex] = {
+    ...student.resume.projects[projectsIndex],
+    ...req.body,
+  };
+  console.log(projectsIndex);
+  await student.save();
+  res.json({ message: "Projects Updated" });
+});
+
+exports.deleteProjects = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  const filteredProjects = student.resume.projects.filter(
+    (i) => i.id !== req.params.projectsid
+  );
+  student.resume.projects=filteredProjects
+  await student.save();
+  res.json({ message: "Projects Deleted" });
+});
+
+exports.addSkills = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  student.resume.skills.push({ ...req.body, id: uuidv4() });
+  await student.save();
+  res.json({ message: "Skills" });
+});
+
+exports.updateSkills = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  const skillsIndex = student.resume.skills.findIndex(
+    (i) => i.id === req.params.skillsid
+  );
+  student.resume.skills[skillsIndex] = {
+    ...student.resume.skills[skillsIndex],
+    ...req.body,
+  };
+  await student.save();
+  res.json({ message: "Skills Updated" });
+});
+
+exports.deleteSkills = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  const filteredSkills = student.resume.skills.filter(
+    (i) => i.id !== req.params.skillsid
+  );
+  student.resume.skills=filteredSkills
+  await student.save();
+  res.json({ message: "Skills Deleted" });
+});
+
+exports.addAccomplishment = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  student.resume.accomplishment.push({ ...req.body, id: uuidv4() });
+  await student.save();
+  res.json({ message: "Accomplishment" });
+});
+
+exports.updateAccomplishment = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  const accomplishmentIndex = student.resume.accomplishment.findIndex(
+    (i) => i.id === req.params.accomplishmentid
+  );
+  student.resume.accomplishment[accomplishmentIndex] = {
+    ...student.resume.accomplishment[accomplishmentIndex],
+    ...req.body,
+  };
+  await student.save();
+  res.json({ message: "Accomplishment Updated" });
+});
+
+exports.deleteAccomplishment = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  const filteredAccomplishment = student.resume.accomplishment.filter(
+    (i) => i.id !== req.params.accomplishmentid
+  );
+  student.resume.accomplishment=filteredAccomplishment
+  await student.save();
+  res.json({ message: "Accomplishment Deleted" });
+});
--- routes/resumeRoutes.js
+++ routes/resumeRoutes.js
@@ -1,27 +1,123 @@
 const express = require("express");
 const router = express.Router();
 const { isAuthenticated } = require("../middleware/auth");
-const { resume, addEducation, updateEducation, deleteEducation, addJobs, updateJobs, deleteJobs } = require("../controllers/resumeController");
+const {
+  resume,
+  addEducation,
+  updateEducation,
+  deleteEducation,
+  addJobs,
+  updateJobs,
+  deleteJobs,
+  addInternship,
+  updateInternship,
+  deleteInternship,
+  addResponsibilities,
+  updateResponsibilities,
+  deleteResponsibilities,
+  addCourses,
+  updateCourses,
+  deleteCourses,
+  addProjects,
+  updateProjects,
+  deleteProjects,
+  addSkills,
+  updateSkills,
+  deleteSkills,
+  addAccomplishment,
+  updateAccomplishment,
+  deleteAccomplishment,
+} = require("../controllers/resumeController");
 
 //GET /
-router.get('/',isAuthenticated,resume);
+router.get("/", isAuthenticated, resume);
 
 //POST /addedu
-router.post('/addedu',isAuthenticated,addEducation);
+router.post("/addedu", isAuthenticated, addEducation);
 
 //POST /edit-edu/:eduid          uuid
-router.post('/edit-edu/:eduid',isAuthenticated,updateEducation);
+router.post("/edit-edu/:eduid", isAuthenticated, updateEducation);
 
 //POST /delete-edu/:eduid          uuid
-router.post('/delete-edu/:eduid',isAuthenticated,deleteEducation);
+router.post("/delete-edu/:eduid", isAuthenticated, deleteEducation);
 
 //POST /addjob
-router.post('/addjob',isAuthenticated,addJobs);
+router.post("/addjob", isAuthenticated, addJobs);
 
 //POST /edit-job/:jobid          uuid
-router.post('/edit-job/:jobid',isAuthenticated,updateJobs);
+router.post("/edit-job/:jobid", isAuthenticated, updateJobs);
 
 //POST /delete-job/:jobid          uuid
-router.post('/delete-job/:jobid',isAuthenticated,deleteJobs);
+router.post("/delete-job/:jobid", isAuthenticated, deleteJobs);
+
+//POST /addinternship
+router.post("/addinternship", isAuthenticated, addInternship);
+
+//POST /edit-internship/:internshipid          uuid
+router.post(
+  "/edit-internship/:internshipid",
+  isAuthenticated,
+  updateInternship
+);
+
+//POST /delete-internship/:internshipid          uuid
+router.post(
+  "/delete-internship/:internshipid",
+  isAuthenticated,
+  deleteInternship
+);
+
+//POST /addresponsibilities
+router.post("/addresponsibilities", isAuthenticated, addResponsibilities);
+
+//POST /edit-responsibilities/:responsibilitiesid          uuid
+router.post(
+  "/edit-responsibilities/:responsibilitiesid",
+  isAuthenticated,
+  updateResponsibilities
+);
+
+//POST /delete-responsibilities/:responsibilitiesid          uuid
+router.post(
+  "/delete-responsibilities/:responsibilitiesid",
+  isAuthenticated,
+  deleteResponsibilities
+);
+
+//POST /addcourses
+router.post("/addcourses", isAuthenticated, addCourses);
+
+//POST /edit-courses/:coursesid          uuid
+router.post("/edit-courses/:coursesid", isAuthenticated, updateCourses);
+
+//POST /delete-courses/:coursesid          uuid
+router.post("/delete-courses/:coursesid", isAuthenticated, deleteCourses);
+
+//POST /addprojects
+router.post("/addprojects", isAuthenticated, addProjects);
+
+//POST /edit-projects/:projectsid          uuid
+router.post("/edit-projects/:projectsid", isAuthenticated, updateProjects);
+
+//POST /delete-projects/:projectsid          uuid
+router.post("/delete-projects/:projectsid", isAuthenticated, deleteProjects);
+
+//POST /addskills
+router.post("/addskills", isAuthenticated, addSkills);
+
+//POST /edit-skills/:skillsid          uuid
+router.post("/edit-skills/:skillsid", isAuthenticated, updateSkills);
+
+//POST /delete-skills/:skillsid          uuid
+router.post("/delete-skills/:skillsid", isAuthenticated, deleteSkills);
+
+//POST /addaccomplishment
+router.post("/addaccomplishment", isAuthenticated, addAccomplishment);
+
+//POST /edit-accomplishment/:accomplishmentid          uuid
+router.post("/edit-accomplishment/:accomplishmentid", isAuthenticated, updateAccomplishment);
+
+//POST /delete-accomplishment/:accomplishmentid          uuid
+router.post("/delete-accomplishment/:accomplishmentid", isAuthenticated, deleteAccomplishment);
 
 module.exports = router;
